61ce1e8c433bfa622fe52ef4d88bd9bf7080592b
Jonathan Logan-Saunders <jonlogansaunders@gmail.com>
2026-08-02 18:54:06 -0400

    Add variables exercise

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables 
+            string name = "BasedJon";
+            int age = 35;
+            double height = 59;
+            bool student = true;
+
+            //Write lines for code above
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+            Console.WriteLine(student);
+
+        }
+    }
+}
