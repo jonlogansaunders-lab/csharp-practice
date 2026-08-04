8feb4746ee965cd8dd970386d82e084440f8ec8e
Jonathan Logan-Saunders <jonlogansaunders@gmail.com>
2026-08-03 22:45:44 -0400

    Create 02-Operators.cs
    Add operators exercise

---
+++ 02-Operators.cs
@@ -0,0 +1,22 @@
+﻿namespace Even_Or_Odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 7;
+            int remainder = number % 2;
+
+            if (remainder == 0)
+            {
+                Console.WriteLine(number + " is even");
+            }
+            else
+
+            {
+                Console.WriteLine(number + " is odd");
+            }
+            
+        }
+    }
+}
