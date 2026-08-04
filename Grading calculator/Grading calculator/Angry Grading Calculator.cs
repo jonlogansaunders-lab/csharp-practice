73cc53013e7e5d96459753360d27f0e77e6b93ae
Jonathan Logan-Saunders <jonlogansaunders@gmail.com>
2026-08-04 01:55:34 -0400

    Angry Grading Calculator
    Calculator with an attitude!

---
+++ Grading calculator/Grading calculator/Angry Grading Calculator.cs
@@ -0,0 +1,30 @@
+﻿namespace Grading_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables for calculator
+            int grade = 100;
+
+            //declare if statements for grading
+            if (grade >= 90)
+            {
+                Console.WriteLine("You passed with an A, smarty pants!");
+            }
+            else if (grade >= 80)
+            {
+                Console.WriteLine("You passed with a B, you could do better!");
+            }
+            else if (grade >= 70)
+            {
+                Console.WriteLine("You barely passed with a C, study ASAP!");
+            }
+            else if (grade <= 69)
+            {
+                Console.WriteLine("You failed, there's no D's in this class");
+            }
+
+        }
+    }
+}
