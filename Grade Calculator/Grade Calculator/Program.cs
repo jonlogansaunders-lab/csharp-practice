0c4b41f1783d0759abc1a966e32d34528e57679a
Jonathan Logan-Saunders <jonlogansaunders@gmail.com>
2026-08-03 23:14:09 -0400

    Grade Calculator

---
+++ Grade Calculator/Grade Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Grade_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int grade = 83;
+
+            if (grade >= 90)
+            {
+                Console.WriteLine("A");
+            }
+            else if (grade >= 80)
+            {
+                Console.WriteLine("B");
+            }
+            else if (grade >= 70)
+            {
+                Console.WriteLine("C");
+            }
+            else
+            {
+                Console.WriteLine("F");
+            }
+        }
+    }
+}
